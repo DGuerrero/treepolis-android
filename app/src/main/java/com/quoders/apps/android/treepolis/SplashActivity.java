6273cd3f41ecd3a8113caaed4b5aff51acebd224
David Guerrero <david.guerrero@quoders.com>
2015-03-29 21:00:21 +0100

    First project commit

---
+++ app/src/main/java/com/quoders/apps/android/treepolis/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.quoders.apps.android.treepolis;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class SplashActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+
+}
