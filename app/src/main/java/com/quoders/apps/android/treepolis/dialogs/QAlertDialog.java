202224a359b118a0ea29f49a758199d039cb8c93
David Guerrero <david.guerrero@quoders.com>
2015-05-10 15:05:12 +0100

    Added progress and alert dialogs helpers

---
+++ app/src/main/java/com/quoders/apps/android/treepolis/dialogs/QAlertDialog.java
@@ -0,0 +1,54 @@
+package com.quoders.apps.android.treepolis.dialogs;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by davidguerrero on 10/04/15.
+ */
+public class QAlertDialog {
+
+    AlertDialog             mDialog = null;
+    Context                 mContext = null;
+
+
+    public QAlertDialog(Context context) {
+
+        mContext = context;
+    }
+
+
+    public void showDialogNeutral(String title, String message, String neutralButtonText,
+                                  DialogInterface.OnClickListener onClickListener) {
+
+        mDialog = new AlertDialog.Builder(mContext).setTitle(title)
+            .setMessage(message)
+            .setNeutralButton(neutralButtonText, onClickListener)
+                .show();
+    }
+
+
+    public void showDialogConfirm(String title, String message,
+                                  String buttonOkText, String buttonCancelText,
+                                  DialogInterface.OnClickListener onClickOk,
+                                  DialogInterface.OnClickListener onClickCancel) {
+
+        mDialog = new AlertDialog.Builder(mContext).setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(buttonOkText, onClickOk)
+                .setNegativeButton(buttonCancelText, onClickCancel)
+                .show();
+    }
+
+
+    public void dismissDialog() {
+
+        if(mDialog != null) {
+            mDialog.dismiss();
+            mDialog = null;
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/quoders/apps/android/treepolis/dialogs/QProgressDialog.java
@@ -0,0 +1,45 @@
+package com.quoders.apps.android.treepolis.dialogs;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+
+/**
+ * Created by davidguerrero on 05/12/14.
+ */
+public class QProgressDialog {
+
+    private ProgressDialog mDialog;
+    private Context mContext;
+
+    public QProgressDialog(Context context) {
+        mContext = context;
+    }
+
+    public void show(String title, String message, boolean cancelable) {
+
+        if(mDialog == null) {
+            mDialog = new ProgressDialog(mContext);
+            mDialog.setCancelable(cancelable);
+            mDialog.setTitle(title);
+            mDialog.setMessage(message);
+            mDialog.show();
+        }
+    }
+
+    public void show(String message, boolean cancelable) {
+
+        if(mDialog == null) {
+            mDialog = new ProgressDialog(mContext);
+            mDialog.setCancelable(cancelable);
+            mDialog.setMessage(message);
+            mDialog.show();
+        }
+    }
+
+    public void stop() {
+        if(mDialog != null) {
+            mDialog.dismiss();
+            mDialog = null;
+        }
+    }
+}
